e13f0e53dec457b819e5a29fe192b9f108301011
Josh9657 <noreply_057@proton.me>
2024-11-24 23:16:00 +0100

    Add files via upload

---
+++ test-osdcloud.ps1
@@ -0,0 +1,19 @@
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI ..."
+start-sleep -Seconds 3
+
+Write-Host -ForegroundColor Yellow "Custom Debug ..."
+Set-ExecutionPolicy RemoteSigned -Force
+Install-Package -Name OSD -Force -SkipPublisherCheck
+
+Write-Host -ForegroundColor Green "Updating OSD Powershell Module"
+Install-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Importing OSD Powershell Module"
+Import-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Start OSD Cloud"
+Start-OSDCloud -OSLanguage fr-fr -OSName 'Windows 11 23H2 x64' -OSLicense Retail -OSEdition Pro -Zti
+
+Write-Host -ForegroundColor Green "Restarting ..."
+start-sleep -Seconds 5
+wpeutil reboot
